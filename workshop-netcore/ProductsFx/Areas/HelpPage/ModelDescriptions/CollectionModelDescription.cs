901051a4be342a8c7e13c395c3c991ae6346b61d
Justin Yoo <justin.yoojh@gmail.com>
2025-07-23 11:34:54 +0900

    Add .NET Fx apps

---
+++ workshop-netcore/ProductsFx/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ProductsFx.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ workshop-netcore/ProductsFx/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ProductsFx.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
