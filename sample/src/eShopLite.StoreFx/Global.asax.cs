4e82550516b10d51c7e46cb691ddb1963af2cc71
Justin Yoo <justin.yoojh@gmail.com>
2025-07-24 16:12:37 +0900

    Add Entity Framework

--- sample/src/eShopLite.StoreFx/Global.asax.cs
+++ sample/src/eShopLite.StoreFx/Global.asax.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+
+using Autofac;
+using Autofac.Integration.Mvc;
+
+using eShopLite.StoreFx.Data;
+using eShopLite.StoreFx.Services;
 
 namespace eShopLite.StoreFx
 {
@@ -12,6 +14,18 @@ namespace eShopLite.StoreFx
     {
         protected void Application_Start()
         {
+            var builder = new ContainerBuilder();
+
+            // Register your MVC controllers
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            // Register other dependencies
+            builder.RegisterType<StoreDbContext>().As<IStoreDbContext>();
+            builder.RegisterType<StoreService>().As<IStoreService>();
+
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
